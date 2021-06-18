b5ccd088b1e9280e1c61f7d8a9b483d2078beaee
denoboi <deniz.kaymakci@hotmail.com>
2021-06-18 18:58:38 +0300

    Devam

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    //public GameObject[] targets2;
+    private float spawnRate = 1;
+    public TextMeshProUGUI textScore;
+    private int score;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        textScore.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator SpawnTarget() // once boyle bir method olusturuyoruz
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(spawnRate); //to control the length of time between each spawn
+            int index = Random.Range(0, targets.Count); //random obje seciyor listeden
+            Instantiate(targets[index]); //random objeyi listeden instantiate ediyor
+        }
+    }    
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse); //yukariya dogru random bolgeye force
+        targetRb.AddTorque (RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse); 
+
+        transform.position = RandomSpawnPos(); 
+    }
+
+    void Update()
+    {
+        
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(12, 15); //yukariya dogru random bolgeye force
+        
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-10, 10); //3 farkli yone objenin donusu
+    }    
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-4, 4), -2); //objenin baslama pozisyonu
+    }
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject); //asagida bir game obje daha var. ona degdiginde yok oluyor..
+    }
+}
